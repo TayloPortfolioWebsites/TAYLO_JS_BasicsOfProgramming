ba897751623c3b4834b0cbba5edec07ff5ee3df5
imhazeltaylo <imhazeltaylo@gmail.com>
2023-04-12 04:10:44 +0800

    Added myName variable with string value

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,6 @@
 var a; //variable declaration
 var b = '10'; //variable initialization
-var ourName; //variable declaration for string
+var ourName;
+var myName = 'Tiff';
 
-console.log(typeof(b));
+console.log(myName);
