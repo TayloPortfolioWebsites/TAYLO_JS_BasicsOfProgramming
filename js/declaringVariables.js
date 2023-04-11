640afb492d6f0f102994b5ba78da15b10fb1bcec
imhazeltaylo <imhazeltaylo@gmail.com>
2023-04-12 07:53:04 +0800

    Add declaringVariables.js example and challenge

---
+++ js/declaringVariables.js
@@ -0,0 +1,11 @@
+var myVar;
+myVar = 15;
+
+var myNum;
+myNum = myVar;
+
+var a;
+a = 7;
+var b;
+
+// console.log(myNum);
--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,12 @@
 var a; //variable declaration
 var b = '10'; //variable initialization
+
 var ourName;
 var myName = 'Tiff';
+
+let a; //variable declaration
+let = 10;
+
+
 
 console.log(myName);
