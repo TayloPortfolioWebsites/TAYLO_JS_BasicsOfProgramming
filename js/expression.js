a3df8e5c05a144f2277a78a64a4bdf9846dd23e3
imhazeltaylo <imhazeltaylo@gmail.com>
2023-04-12 09:00:41 +0800

    solved the expressions challenge

---
+++ js/expression.js
@@ -0,0 +1,14 @@
+// WHAT ARE THE THREE EXPRESSIONS IN HERE?
+
+let a; 
+let b = 3; 
+let c = 2;
+let d = b+c;
+
+/*
+1. let a; - variable declaration
+2. a = b+c - variable initialization
+3. b+c - performing evaluation
+*/
+
+console.log(d);
