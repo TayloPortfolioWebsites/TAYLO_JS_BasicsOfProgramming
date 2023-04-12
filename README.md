c2280f610a136451eee3a67edb6a7754ea7bae58
imhazeltaylo <imhazeltaylo@gmail.com>
2023-04-12 08:16:06 +0800

    Added string literal challenge

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TAYLO_JS_BasicsOfProgramming
+
--- js/declaringVariables.js
+++ js/declaringVariables.js
@@ -5,11 +5,23 @@ var myNum;
 myNum = myVar;
 
 //CODING CHALLENGE
-var a;
+/*var a;
 a = 7;
 var b;
 //Hazel Tiffany Taylo
 b = a;
-console.log(b);
+console.log(b);*/
 
 // console.log(myNum);
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+// reinitialize variable a and add a value of 1
+a = a+1;
+// reinitialize variable b and add a value of 5
+b = b+5;
+// reinitialize variable c and concatenate "string!";
+c = c+" string!";
+
+console.log(c);
