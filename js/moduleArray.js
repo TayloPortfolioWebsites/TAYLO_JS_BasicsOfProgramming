2bdf8e4b170973aff4c0fe2d84a89f2e900a277c
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-04-15 07:07:14 +0800

    Add moduleArray examples

---
+++ js/moduleArray.js
@@ -0,0 +1,15 @@
+let a = [4,8,1,16,23,54];
+let b = ['rod','josep','jash','amy'];
+let c = [40, 'rod', true];
+let d = ['ana','rose','mary','jane'];
+let e = [10,20,30,40,50,24];
+
+// console.log(a[0]);
+// console.log(a);
+// a[6]=30; number 30 was added to the end of the a array
+// array's type is object
+// console.log(d.length);
+// e.push(54); add a value at the end of the array
+// e.pop(); remove a value at the end of the array
+
+console.log(e);
