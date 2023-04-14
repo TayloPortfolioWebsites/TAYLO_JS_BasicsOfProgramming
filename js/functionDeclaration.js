e23136d239213ec2ef3cb40ea612392df5b223a5
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-04-15 07:47:44 +0800

    Added functionDeclaration examples

---
+++ js/functionDeclaration.js
@@ -0,0 +1,20 @@
+// console.log()
+// method.log()
+
+// function sayHello(){
+//     var name;
+//     console.log('Hello World');
+// }
+
+// function sayHello2(){
+//     let answer = prompt("Enter your name here: ");
+//     console.log('Hello ' + answer + '!');
+// }
+
+// function calculateTax(amount){
+//     let result = amount * 0.825;
+//     return result;
+// }
+
+// let tax = calculateTax(100);
+// console.log(tax);
