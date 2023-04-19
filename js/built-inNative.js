dd0067c14e5d3ec6c2f8ccf4dfcee6bebfaba48b
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-04-19 09:39:53 +0800

    Add built-inNative.js examples

---
+++ js/built-inNative.js
@@ -0,0 +1,30 @@
+// let pattern = /xyz/;
+// let value = 'This is xyz a test to replace the value of pattern.';
+// console.log(value.replace(pattern,'hero'));
+// console.log(typeof pattern);
+
+/**************************************************************** */
+
+// let myString = new String('howdy');
+// console.log(myString);
+// console.log(myString.toString());
+
+/**************************************************************** */
+
+// let myPrimitive = 'This is PRIMITIVE data type';
+// console.log(typeof myPrimitive);
+// myPrimitive = myPrimitive.toLowerCase();
+// console.log(myPrimitive);
+// console.log(typeof myPrimitive);
+
+/**************************************************************** */
+
+let myNumber = new Number(7);
+console.log(typeof myNumber);
+
+let myPrimitive2 = myNumber.valueOf();
+console.log(myPrimitive2);
+console.log(typeof myPrimitive2);
+
+/**************************************************************** */
+
