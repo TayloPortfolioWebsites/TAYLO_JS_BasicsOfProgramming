2b1f7d49964be0b4ebede8f7a9c10e3ab76016ba
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-04-19 08:26:28 +0800

    Add modulePattern.js examples

---
+++ js/modulePattern.js
@@ -0,0 +1,44 @@
+var counter = (function(){
+    let count = 0;
+
+    function print(message){
+        console.log(message + '---' + count);
+    }
+
+    return{
+        value: count,
+        get: function(){
+            return count;
+        },
+        set: function(value){
+            count = value;
+        },
+        increment: function(){
+            count += 1;
+            print('After Increment');
+        },
+        reset: function(){
+            print('Before reset: ');
+            count = 0;
+            print('After reset: ');
+        }
+    }
+})();
+
+// console.log(counter.count);
+// console.log(counter.value);
+// console.log(counter.increment());
+// console.log(counter.increment());
+// console.log(counter.reset());
+// console.log(counter.increment());
+
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+
+counter.set(7);
+console.log(counter.get());
+counter.reset();
+
+/*************************************************** */
