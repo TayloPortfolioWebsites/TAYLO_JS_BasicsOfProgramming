039eec340418b77b4b31c2ad1b79b20f418ab72f
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-04-21 15:30:08 +0800

    Add rest.js examples

--- js/rest.js
+++ js/rest.js
@@ -1,12 +1,21 @@
-function checkout(...items){
-    let total = 0;
-    for(let i in items){
-        total += items[i];
-    }
-    return total;
+// function checkout(...items){
+//     let total = 0;
+//     for(let i in items){
+//         total += items[i];
+//     }
+//     return total;
+// }
+
+// console.log("The total is: $" + checkout(10,40,30));
+
+/**************************************************** */
+
+function displayName(first, last, ...middle){
+    console.log("Welcome " + first, middle.join(), last + " to our game!");
 }
 
-console.log("The total is: $" + checkout(10,40,30));
+// console.log(displayName("Hazel Tiffany","Taylo","Cabanero"));
+displayName("Hazel Tiffany","Taylo","Cabanero");
 
 /**************************************************** */
 
