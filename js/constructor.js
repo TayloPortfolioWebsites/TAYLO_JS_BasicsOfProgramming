66c8f668e65b184148ce7915f15fdec6aa535ea5
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-04-21 15:01:38 +0800

    Added examples for constructor.js

---
+++ js/constructor.js
@@ -0,0 +1,30 @@
+// let car = {
+//     make: 'bmw',
+//     model: '745li',
+//     year: 2000
+// }
+
+/**************************************************** */
+
+// function Car(make, model, year){
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
+// }
+
+// let myCar = new Car('bmw', '745li', 2023);
+
+// console.log(myCar);
+
+/**************************************************** */
+
+function myFunction(){
+    console.log("Hey, we've just created a new function.");
+}
+
+let MyFunction = new myFunction();
+console.log(MyFunction);
+console.log(myFunction);
+console.log(typeof MyFunction);
+
+/**************************************************** */
